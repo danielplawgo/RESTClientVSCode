d07edada33c4e803119a51ef0cf03410732f8256
Daniel Plawgo <daniel@plawgo.pl>
2021-04-12 05:50:21 +0200

    dodanie przykładu

---
+++ RESTClientVSCode/RESTClientVSCode/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RESTClientVSCode.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
